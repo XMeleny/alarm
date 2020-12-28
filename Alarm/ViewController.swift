20e2d97c7687666f21fea8c2af9ecc68a9bb25a2
zhuxiaomei <865622793@qq.com>
2020-12-28 17:24:21 +0800

    local notification

--- Alarm/ViewController.swift
+++ Alarm/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func requestPermission(){
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            if granted {
+                self.doAdd()
+            }else{
+                self.cannotAdd()
+            }
+        }
+    }
+    
+    func addNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .notDetermined {
+                self.requestPermission()
+            }else if settings.authorizationStatus == .authorized || settings.authorizationStatus == .provisional {
+                self.doAdd()
+            } else{
+                self.cannotAdd()
+            }
+        }
+    }
+    
+    func doAdd(){
+        let content = UNMutableNotificationContent()
+        content.title = "this is title"
+        content.body = "this is body"
+        
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        dateComponents.hour = 17
+        dateComponents.minute = 7
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let uuid = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if let error = error {
+                print(error)
+            }
+        }
+        print("add successfully")
+    }
+    
+    func cannotAdd(){
+        // todo, alert or jump to settings page. question is how to jump to settings page
+        print("not granted, pls reRequest")
+    }
+    
+    @IBAction func sendClick(_ sender: UIButton) {
+        requestPermission()
+    }
 }
-
