4b53ace2a32d4bb3ec7ab2643d8de2bfb9402506
zhuxiaomei <865622793@qq.com>
2021-01-04 15:16:18 +0800

    update memo

--- Alarm/EditViewController.swift
+++ Alarm/EditViewController.swift
@@ -49,21 +49,24 @@ class EditViewController: UIViewController{
         }else{
             let date = dateInput.date
             let time = timeInput.date
-           
+            
             let calendar = Calendar.current
             
             let year = calendar.component(.year, from: date)
             let month = calendar.component(.month, from: date)
             let day = calendar.component(.day, from: date)
-            
             let hour = calendar.component(.hour, from: time)
             let minute = calendar.component(.minute, from: time)
             
-//            print("date = \(date), time = \(time)")
-//            print("year = \(year), month = \(month), day = \(day), hour = \(hour), minute = \(minute)")
-            let memo = Memo(content: content!, year: year, month: month, day: day, hour: hour, minute: minute)
-            
-            addMemo(memo: memo)
+            switch inputType {
+            case .add:
+                let memo = Memo(content: content!, year: year, month: month, day: day, hour: hour, minute: minute)
+                addMemo(memo: memo)
+                break
+            case .modify:
+                updateMemo(id: inputMemo!.id, content: content!, year: year, month: month, day: day, hour: hour, minute: minute)
+                break
+            }
             navigationController?.popViewController(animated: true)
         }
     }
